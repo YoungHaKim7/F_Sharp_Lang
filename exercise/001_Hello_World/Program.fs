e4ac5d59a35ea2c4678df54c9c28c792dc7adea5
YoungHa Kim <ytok1108@aol.com>
2022-12-04 23:22:14 +0900

    F # exercise 01

---
+++ exercise/001_Hello_World/Program.fs
@@ -0,0 +1,16 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+open System
+
+let printSomeText() =
+    let text = "Hello" + "World"
+    printfn $"text = {text}"
+
+let showCommandLineArgs() =
+    for arg in Environment.GetCommandLineArgs() do
+        printfn $"arg = {arg}"
+
+printSomeText()
+showCommandLineArgs()
+exit 100
+  
