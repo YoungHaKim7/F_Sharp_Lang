1856840678e77d47bb6de9a44dfc804325a6482b
unknown <ytok1108@aol.com>
2022-12-06 18:16:33 +0900

    F# style print

---
+++ exercise/003_Expression_based/Program.fs
@@ -0,0 +1,18 @@
+// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let x = 
+        let x = 5
+        let y = 10
+        x + y
+    let printNumber number =
+        printfn "%i" number
+        
+    printNumber x
+    0 // return an integer exit code
+    
+// Result:
+15
