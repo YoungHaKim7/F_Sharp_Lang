2251c7f5b8df6bcc7cdf4345bfae1b3e06435bb3
YoungHa Kim <ytok1108@aol.com>
2022-12-10 15:59:54 +0900

    F# with the .NET CLI

---
+++ dotnet_sln_add_sample/FSarpSample/src/App/Program.fs
@@ -0,0 +1,15 @@
+﻿
+
+// For more information see https://aka.ms/fsharp-console-apps
+open System
+open Library
+
+[<EntryPoint>]
+let main args =
+  printfn "Nice command-line arguments! Here's waht System.Text.Json has to say about them: "
+
+  let value, json = getJson {| args=args; year=System.DateTime.Now.Year|}
+  printfn $"Input: %0A{value}"
+  printfn $"Output: %s{json}"
+
+  0 // return an integer exit code
---
+++ dotnet_sln_add_sample/README.md
@@ -0,0 +1,3 @@
+# Get started with F# with the .NET CLI
+
+https://learn.microsoft.com/en-us/dotnet/fsharp/get-started/get-started-command-line?source=recommendations
